f99582b2e6a5e162c24350827174afda26dbcd24
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-03-30 12:17:47 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Yo, Don't cheat. I got my eye on you...")
+
+function start() {
+  document.getElementById('screen').innerHTML = "<p>Bruh, the dev never made this part yet.</p>";
+}
+
+function credits() {
+  document.getElementById('screen').innerHTML = "<p>mainly mariocraft987 on <a href='https://github.com/mariocraft987'>github.</a></p>";
+}
