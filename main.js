247afebd6d60cab325c49a23ea4ca23407fdb54a
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-03-30 12:19:36 -0700

    GET GOING "S"!!

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-console.log("Yo, Don't cheat. I got my eye on you...")
+console.log("Yo, Don't cheat. I got my eye on you...");
 
 function start() {
   document.getElementById('screen').innerHTML = "<p>Bruh, the dev never made this part yet.</p>";
 }
 
-function credits() {
+function credit() {
   document.getElementById('screen').innerHTML = "<p>mainly mariocraft987 on <a href='https://github.com/mariocraft987'>github.</a></p>";
 }
